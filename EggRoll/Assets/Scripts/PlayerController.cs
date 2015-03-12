d5b6ca95c437bf163b2d1ff2efbd180e686c45f7
Cy Eitharong <cyeitharong@gmail.com>
2015-03-12 11:34:52 -0700

    Roll an egg

---
+++ EggRoll/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+	public float acceleration;
+	private int count;
+
+	void Start ()
+	{
+		count = 0;
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		var movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce(movement * acceleration * Time.deltaTime);
+		
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
